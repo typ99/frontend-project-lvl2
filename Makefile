7f42ce6b66ef2bc567c42c03f85860f337ab6058
typ99 <superarkada@gmail.com>
2022-02-19 19:20:58 +0300

    f

--- Makefile
+++ Makefile
@@ -1,8 +1,18 @@
 install:
 	npm ci
-
+stringify:
+	node bin/gendiff.js __fixtures__/file1.json __fixtures__/file2.json
+json:
+	node bin/gendiff.js --format json __fixtures__/file1.json __fixtures__/file2.json
+plain:
+	node bin/gendiff.js --format plain __fixtures__/file1.json __fixtures__/file2.json
+yml:
+	node bin/gendiff.js __fixtures__/file1.yml __fixtures__/file2.yml
+lint:
+	npx eslint --fix .
+test:
+	npm test
+test-coverage:
+	npm test -- --coverage --coverageProvider=v8
 publish:
 	npm publish --dry-run
-
-lint:
-	npx eslint .
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,11 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
+  .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, { format }) => {
     console.log(genDiff(filepath1, filepath2, format));
   });
-
 program.parse();
